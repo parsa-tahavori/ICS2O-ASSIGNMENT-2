e9547aac3e708a4aaacfc46f6d4bfad14bc7ffbe
parsa-tahavori <113055869+parsa-tahavori@users.noreply.github.com>
2022-10-20 00:16:15 +0000

    Correct way

---
+++ js/script.js
@@ -0,0 +1,17 @@
+'use strict'
+/**
+ * This function calculates area and perimeter of kite.
+ */
+function calculate () {
+  // input
+  const hight = parseInt(document.getElementById('hight-of-kite').value)
+  const base = parseInt(document.getElementById('base-of-kite').value)
+
+  // process
+  const area = hight * base / 2
+  const perimeter = 2 * (hight + base)
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+  document.getElementById('perimeter').innerHTML = 'Perimeter is: ' + perimeter + ' cm'
+}
